753071075b342ab2327a44b44431a29df7d1b330
Raxit910Bac <raxit.sheladiya@bacancy.com>
2025-02-17 10:57:21 +0530

    Task 3 of Week 2 Added.

---
+++ Week-2 Tasks/w2task3.js
@@ -0,0 +1,12 @@
+// Task 3 - IIFE (Immediately Invoked Function Expression)
+
+// Create simple function that will run only once while we start program.
+
+(function () {
+  console.log("This function runs only once at the start!");
+})();
+
+
+// Notes: 
+// - This function executes immediately when the program starts.
+// - It does not get called again because it's not assigned to any variable.
