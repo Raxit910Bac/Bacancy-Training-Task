522ad9d297ec0920bee659fed5a211a8ebe758cf
Raxit910Bac <raxit.sheladiya@bacancy.com>
2025-02-20 15:10:30 +0530

    Task 2 of Week 3 Updated.

---
+++ Week - 3 Tasks/W3 - Task2/simplescript.js
@@ -0,0 +1,6 @@
+// Create sample script named simplescript.js that prints current date and time and executes using node. print current time using momentjs library.
+
+
+import moment from "moment";
+
+console.log("Current Date and Time:", moment().format("YYYY-MM-DD HH:mm:ss"));
